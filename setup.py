7006b00862cb86c91f6e190636737b129ba19bfb
Zowie <zowielangdon@gmail.com>
2015-07-06 17:44:17 +0200

    Update setup.py
    Fix encoding issue when installing with Pip3

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
 from setuptools import setup, find_packages
 
+import sys
+
 __version__ = None
 with open('sgbackend/version.py') as f:
     exec(f.read())
+
+PY_VERSION = sys.version_info[0], sys.version_info[1]
+
+if PY_VERSION < (3, 0):
+    long_description = open('README.rst').read()
+else:
+    long_description = open('README.rst', encoding='utf-8').read()
 
 setup(
     name='sendgrid-django',
@@ -13,6 +22,6 @@ setup(
     packages=find_packages(),
     license='MIT',
     description='SendGrid Backend for Django',
-    long_description=open('./README.rst').read(),
+    long_description=long_description,
     install_requires=["sendgrid==1.4.0"],
 )
